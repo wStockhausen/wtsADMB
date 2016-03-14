fda1e82dd5a02465ff9d55c9b5b76417fd6247c9
wStockhausen <william.stockhausen@noaa.gov>
2016-03-14 10:30:17 -0700

    Added includes for <limits> and <iostream> to BasicFunctions.

--- include/wtsADMB.hpp
+++ include/wtsADMB.hpp
@@ -10,16 +10,17 @@
  * 
  */
 
+#pragma once
 #ifndef WTSADMB_HPP
 #define	WTSADMB_HPP
 
 #include "wtsConsts.hpp"
-#include "admbParamFunctions.hpp"
 #include "admbBasicFunctions.hpp"
+#include "admbFileFunctions.hpp"
+#include "admbParamFunctions.hpp"
 #include "admbProbFunctions.hpp"
 #include "admbStdFunctions.hpp"
 #include "admbStringFunctions.hpp"
-#include "admbFileFunctions.hpp"
 #include "rFunctions.hpp"
 
 #endif	/* WTSADMB_HPP */
